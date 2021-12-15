0ce67715efd5bbb580f2bc84ce9250feb3a8bc20
whyRuRunnin <popescu.robert.schmidt@gmail.com>
2021-12-15 20:06:40 +0100

    added timer to different file

--- mine.js
+++ mine.js
@@ -218,39 +218,3 @@ function restartGame() {
 // buttons 
 document.getElementById("resetGame").addEventListener("click", restartGame);
 document.getElementById("finishGame").addEventListener("click", validate);
-
-// Timer 
-let seconds = document.getElementById("second");
-let minutes = document.getElementById("minute");
-seconds.innerHTML = 0;
-minutes.innerHTML = 0;
-
-let sec = 1;
-let min = 1;
-let cron;
-
-function myTimer() {
-    if (seconds.innerHTML > 58) {
-        sec = 0;
-        minutes.innerHTML = min;
-        ++min;
-    }
-    seconds.innerHTML = sec;
-    sec++;
-}
-
-function stop() {
-    clearInterval(cron);
-    cron = null;
-}
-
-function resetTimer() {
-    seconds.innerHTML = 0;
-    minutes.innerHTML = 0;
-    sec = 1;
-    min = 1;
-}
-
-function start() {
-    cron = setInterval(myTimer, 1000);
-}
